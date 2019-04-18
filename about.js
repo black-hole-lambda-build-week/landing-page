4b63411bf43de3d92f1aa288b3b990e0a6d55f5f
amlane <twelvexstring@gmail.com>
2019-04-18 16:56:12 -0500

    debuggeed component for about us page to allow content to fade in when clicking on image

--- about.js
+++ about.js
@@ -1,28 +1,23 @@
 //dropdown 
-class Member {
+class MemberInfo {
     constructor(member) {
         this.member = member;
-        console.log(this)
+        this.selectMember = this.member.querySelector('img');
+        this.memberContent = this.member.querySelector('.member-content');
+        
+        this.toggleMember = this.toggleMember.bind(this);
+        this.selectMember.addEventListener('click', this.toggleMember);
+    }
+    //methods
+    toggleMember() {
+        this.memberContent.classList.toggle('member-content');
+        this.member.classList.toggle('fadeIn');
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //step 1 
 const teamMember = document.querySelectorAll('.member')
 
 teamMember.forEach((member) => {
-    return new Member(member);
+    return new MemberInfo(member);
 });
